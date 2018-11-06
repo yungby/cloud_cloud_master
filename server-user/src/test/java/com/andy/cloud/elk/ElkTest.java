b27b57df0ab68e51fdf028af23cb28e0a2fa5e49
yungby <yungbyzy@163.com>
2018-11-06 14:39:43 +0800

    init

--- server-user/src/test/java/com/andy/cloud/elk/ElkTest.java
+++ server-user/src/test/java/com/andy/cloud/elk/ElkTest.java
@@ -2,7 +2,6 @@ package com.andy.cloud.elk;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.test.context.TestComponent;
 import org.springframework.stereotype.Component;
 
 /**
